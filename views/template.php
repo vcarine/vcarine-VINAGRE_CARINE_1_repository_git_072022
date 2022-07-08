f1f7a09a9e5c02cae69e138c196d8551c73f6875
vcarine <you@example.com>
2022-07-08 17:25:32 +0200

    routing views

--- views/template.php
+++ views/template.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="public/style.css">
     <!--    bootstrap -->
     <link rel="stylesheet" href="https://bootswatch.com/5/sandstone/bootstrap.min.css">
-
+    <!--    fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/fontawesome.min.css">
 
 
 </head>
@@ -27,10 +28,10 @@
         <div class="collapse navbar-collapse  " id="navbarColor01">
             <ul class="navbar-nav w-100 d-flex justify-content-start ">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Accueil</a>
+                    <a class="nav-link active" href="accueil">Accueil</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="articles.php">Article</a>
+                    <a class="nav-link" href="articles">Article</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Contact</a>
@@ -41,7 +42,7 @@
             </ul>
             <ul class="navbar-nav w-100 d-flex justify-content-end">
                 <li class="nav-item">
-                    <a class="nav-link active" href="index.php">Connexion</a>
+                    <a class="nav-link active" href="../index.php">Connexion</a>
                 </li>
             </ul>
         </div>
@@ -57,8 +58,8 @@
 
 <!--    js -->
 <script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" ></script>
 <script src="app.js"></script>
 </body>
 </html>
