1e2b6506fa3452ad9d43d557d7b31e88161c3a87
vcarine <you@example.com>
2022-07-29 16:17:10 +0200

    created router

---
+++ src/index.php
@@ -0,0 +1,117 @@
+<?php
+
+//use App\controllers\ArticlesController;
+//use App\controllers\Toolbox;
+//use App\controllers\User\UserController;
+//
+//
+
+require 'vendor/autoload.php';
+
+// initaliser la class
+$router = new App\Router($_GET['url']);
+
+// Comme une API
+
+// Qd j'appel , url (/post) , je veux que tu lances une fonction qui va afficher tous les articles
+
+$router->get('/posts', function(){echo 'Tous les articles';});
+$router->get('/posts/: id', function($id){echo 'Afficher l\'article' .$id;});
+$router->post('/posts/: id', function($id){echo 'Poster pour l\'article' .$id;});
+
+
+
+
+
+
+
+
+//define('URL', str_replace("index.php", "", (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF']));
+//
+//
+//try {
+//    if (empty($_GET['page'])) {
+//        require "Views/accueil.view.php";
+//    } else {
+//        $url = explode("/", filter_var($_GET['page']), FILTER_SANITIZE_URL);
+//        match ($url[0]) {
+//            'accueil' => require "Views/accueil.view.php",
+//            'articles' => getDisplayArticle(),
+//            'article' => actionArticle($url[1], $url[2]),
+//
+//            default => throw new Exception("La page n'existe pas"),
+//        };
+//    }
+//} catch
+//(Exception $e) {
+//    echo $e->getMessage();
+//}
+///**
+// * @return void
+// */
+//function getDisplayArticle(): void
+//{
+//    $articles = new ArticlesController();
+//    $articles->displayArticles();
+//}
+//
+///**
+// * @param string $parameter
+// * @param int $id
+// *
+// * @return void
+// *
+// * @throws Exception
+// */
+//function actionArticle(string $parameter, int $id): void
+//{
+//    $articles = new ArticlesController();
+//    if ($parameter === "s") {
+//        $articles->showArticle($id);
+//    } else if ($parameter === "a") {
+//        $articles->addArticle();
+//    } else if ($parameter === "e") {
+//        $articles->editArticle();
+//    } else if ($parameter === "d") {
+//        $articles->deleteArticle($id);
+//    } else {
+//        throw new Exception("La page n'existe pas");
+//    }
+//}
+//
+//$userController = new UserController();
+//
+//try {
+//    if(empty($_GET['page'])){
+//        $page = "accueil";
+//    } else {
+//        $url = explode("/", filter_var($_GET['page'],FILTER_SANITIZE_URL));
+//        $page = $url[0];
+//    }
+//
+//    switch($page){
+//        case "article" : $userController->article();
+//            break;
+//        case "login" : $userController->login();
+//            break;
+//        case "validation_login" :
+//            if(!empty($_POST['login']) && !empty($_POST['password'])){
+//                $login = Securite::secureHTML($_POST['login']);
+//                $password = Securite::secureHTML($_POST['password']);
+//                $userController->validation_login($login,$password);
+//            } else {
+//                Toolbox::addMessageAlerte("Login ou mot de passe non renseigné", Toolbox::COULEUR_ROUGE);
+//                header('Location: '.URL."login");
+//            }
+//            break;
+//        case "compte" :
+//            switch($url[1]){
+//                case "profil": $userController->article();
+//                    break;
+//            }
+//            break;
+//        default : throw new Exception("La page n'existe pas");
+//    }
+//} catch (Exception $e){
+//    $userController->pageError($e->getMessage());
+//}
